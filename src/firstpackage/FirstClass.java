82a9dcbea4899aeb18a7b907758e541a0c1cbdf7
Baki <smk633044@gmail.com>
2020-08-16 22:32:06 +0200

    son conflict bugun icin

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -7,7 +7,7 @@ public class FirstClass {
 	System.out.println("second commit from local repository");	
 
 
-	System.out.println("conflict yapmak istiyoruz");
+	System.out.println("2. kez conflict yapiyoruz");
 
 	System.out.println("first commit from remote repository");
 
